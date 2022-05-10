3a520c3efef2811714c278928452df45892459ed
Martin Burger <mburger@kochag.ch>
2022-05-10 15:18:05 +0200

    initial commit

---
+++ QAMGen/main.c
@@ -0,0 +1,64 @@
+/*
+ * QAMGen.c
+ *
+ * Created: 20.03.2018 18:32:07
+ * Author : chaos
+ */ 
+
+//#include <avr/io.h>
+#include "avr_compiler.h"
+#include "pmic_driver.h"
+#include "TC_driver.h"
+#include "clksys_driver.h"
+#include "sleepConfig.h"
+#include "port_driver.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "event_groups.h"
+#include "stack_macros.h"
+
+#include "mem_check.h"
+
+#include "init.h"
+#include "utils.h"
+#include "errorHandler.h"
+#include "NHD0420Driver.h"
+
+#include "qaminit.h"
+#include "qamgen.h"
+
+
+static long count = 0;
+
+extern void vApplicationIdleHook( void );
+
+void vApplicationIdleHook( void )
+{	
+	count++;
+}
+
+int main(void)
+{
+	resetReason_t reason = getResetReason();
+
+	vInitClock();
+	vInitDisplay();
+	initDAC();
+	initDACTimer();
+	initGenDMA();
+	//initADC();
+	//initADCTimer();
+	//initDecDMA();
+	xTaskCreate(vQuamGen, NULL, configMINIMAL_STACK_SIZE+500, NULL, 2, NULL);
+
+	vDisplayClear();
+	vDisplayWriteStringAtPos(0,0,"FreeRTOS 10.0.1");
+	vDisplayWriteStringAtPos(1,0,"EDUBoard 1.0");
+	vDisplayWriteStringAtPos(2,0,"QAMGEN-Base");
+	vDisplayWriteStringAtPos(3,0,"ResetReason: %d", reason);
+	vTaskStartScheduler();
+	return 0;
+}
+
